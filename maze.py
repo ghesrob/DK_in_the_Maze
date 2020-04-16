17a3597dda9103db4ff8ff33d5767b1fe94328dc
ghesrob <58829098+ghesrob@users.noreply.github.com>
2020-04-16 05:10:29 +0200

    New maze generator

--- maze.py
+++ maze.py
@@ -1,85 +1,66 @@
+import numpy as np
 import pygame
 from pygame.locals import *
 import random 
 
 # Fichier local
 from constantes import *
-from cell import Cell
 
 class Maze:
-    """ Classe permettant de générer aléatoirement un labyrinthe 
-    à l'aide de l'algorithme first depth search """
 
-    def __init__(self, nx, ny, ix=0, iy=0):
-        """Initialize the maze grid.
-        The maze consists of nx x ny cells and will be constructed starting
-        at the cell indexed at (ix, iy).
-        """
-        self.nx, self.ny = nx, ny
-        self.ix, self.iy = ix, iy
-        self.maze_map = [[Cell(x, y) for y in range(ny)] for x in range(nx)]
-        self.structure = []
-
-    def cell_at(self, x, y):
-        """Return the Cell object at (x,y)."""
-        return self.maze_map[x][y]
+    def __init__(self, length_x, length_y, start_x = 1, start_y = 1):
+        self.length_x, self.length_y = length_x, length_y
+        self.start_x, self.start_y = start_x, start_y
+        self.structure = np.full(fill_value = 'm', shape = (length_x, length_y))
 
 
-    def find_valid_neighbours(self, cell):
-        """Return a list of unvisited neighbours to cell."""
+    def find_valid_ways(self, coord_x, coord_y):
+        """ Méthode qui détermine les directions valides à partir de la cellule actuelle """
+        
+        delta = np.array([
+            ['up', (0,-1), (0,-2)],
+            ['down', (0,1), (0,2)],
+            ['left', (-1,0), (-2,0)],
+            ['right', (1,0), (2,0)]
+            ])     
+        valid_ways = []
+        for direction, (d1x, d1y), (d2x, d2y) in delta:           
+            mid_x, mid_y = coord_x + d1x, coord_y + d1y
+            end_x, end_y = coord_x + d2x, coord_y + d2y
+            if 0 < end_x < self.length_x and 0 < end_y < self.length_y:
+                if self.structure[end_x, end_y] == 'm':
+                    valid_ways.append((direction, (mid_x, mid_y), (end_x, end_y)))  
+        return(valid_ways)
 
-        delta = [('W', (-1,0)),
-                 ('E', (1,0)),
-                 ('S', (0,1)),
-                 ('N', (0,-1))]
-        neighbours = []
-        for direction, (dx,dy) in delta:
-            x2, y2 = cell.x + dx, cell.y + dy
-            if (0 <= x2 < self.nx) and (0 <= y2 < self.ny):
-                neighbour = self.cell_at(x2, y2)
-                if neighbour.has_all_walls():
-                    neighbours.append((direction, neighbour))
-        return neighbours
 
 
-    def make_maze(self):
-        # Total number of cells.
-        n = self.nx * self.ny
-        cell_stack = []
-        current_cell = self.cell_at(self.ix, self.iy)
-        # Total number of visited cells during maze construction.
-        nv = 1
+    def create(self):
+        """ Méthode qui génère le labyrinthe """
+        
+        current_cell = (self.start_x, self.start_y)
+        self.structure[current_cell] = '0'
 
-        while nv < n:
-            neighbours = self.find_valid_neighbours(current_cell)
+        cell_stack = [current_cell]
 
-            if not neighbours:
-                # We've reached a dead end: backtrack.
-                current_cell = cell_stack.pop()
+        while cell_stack:
+            valid_ways = self.find_valid_ways(*current_cell)
+
+            if not valid_ways:
+                cell_stack.pop()
+                if cell_stack:
+                    current_cell = cell_stack[-1]
                 continue
 
-            # Choose a random neighbouring cell and move to it.
-            direction, next_cell = random.choice(neighbours)
-            current_cell.knock_down_wall(next_cell, direction)
+            # Choisit une destination, et s'y déplace
+            direction, mid_cell, end_cell = random.choice(valid_ways)
+            self.structure[mid_cell] = '0'
+            self.structure[end_cell] = '0'
+            current_cell = end_cell
             cell_stack.append(current_cell)
-            current_cell = next_cell
-            nv += 1
-
-    def large_maze(self):
-        large = [["X" if x in [0, 2*self.nx] or y in [0, 2*self.ny] else "O" for y in range(2 * self.ny + 1)] for x in range(2 * self.nx + 1)]
-
-        for y in range(self.ny):
-            for x in range(self.nx):
-                if self.cell_at(x, y).walls["S"]:
-                    large[2*x+1][2*(y+1)] = "X"
-                if self.cell_at(x, y).walls["E"]:
-                    large[2*(x+1)][2*y+1] = "X"
-                large[2*x][2*y] = "X"
-        self.structure =  list(map(list, zip(*large)))
-
 
     def show(self, window):
         """ Méthode qui affiche le niveau à l'écran """
+
         # Chargement des images
         wall_image = pygame.image.load(wall).convert_alpha()
         start_image = pygame.image.load(start).convert_alpha()
@@ -90,8 +71,12 @@ class Maze:
         for row in self.structure:
             pixel_x = 0
             for cell in row:
-                if cell == "X":
+                if cell == "m":
                     window.blit(wall_image, (pixel_x, pixel_y))
+                elif cell == "d":
+                    window.blit(start_image, (pixel_x, pixel_y))
+                elif cell == "a":
+                    window.blit(banana_image, (pixel_x, pixel_y))                    
                 pixel_x += cell_size
             pixel_y += cell_size
       
