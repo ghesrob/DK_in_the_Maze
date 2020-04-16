b98b054ccc545a2f1d126477de0037280b19b770
ghesrob <58829098+ghesrob@users.noreply.github.com>
2020-04-16 05:16:36 +0200

    Update constantes.py

--- constantes.py
+++ constantes.py
@@ -1,5 +1,5 @@
 #Paramètres de la fenêtre
-cell_count = 15
+cell_count = 25
 cell_size = 30
 window_size = cell_count * cell_size
 
@@ -13,7 +13,7 @@ dk_main = "sprites/dk_main.jpg"
 start = "sprites/start.png"
 banana = "sprites/banana.png"
 wall = "sprites/wall.png"
-background = "sprites/background.jpg"
+background = "sprites/background750.jpg"
 
 # Sprites du personnage
 dk_up = "sprites/dk_up.png"
@@ -21,6 +21,7 @@ dk_down = "sprites/dk_down.png"
 dk_left = "sprites/dk_left.png"
 dk_right = "sprites/dk_right.png"
 
-# Niveaux de jeu 
+# Niveaux de jeu - Obsolète, ne sert maintenant qu'à des fins de débugage.
+## TODOO : il faut supprimer le bloc de selection de niveau, qui appelle encore ces objets
 level_1 = "levels/level_1.txt"
 level_2 = "levels/level_2.txt"
